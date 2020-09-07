1d17f23e229627d216cd1c9e506886afc109a0e0
Su1tan05 <67367027+Su1tan05@users.noreply.github.com>
2020-09-07 20:36:23 +0300

    example1

---
+++ Example1/Example1/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Example1.Models;  
+using System.Linq;
+
+namespace Example1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            int minute = DateTime.Now.Minute;
+            ViewBag.Greeting = hour + ":" + minute;
+            ViewBag.Sultan = hour < 12 ? "Good Morning" : "Good Afternoon";
+            if (hour < 12)
+            {
+                ViewBag.Massage = "Доброе утро";
+            }
+            else
+            {
+                ViewBag.Massage = "Добрый вечер";
+            }
+            return View("MyView");
+        }
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            Repasitory.AddResponse(guestResponse);
+            return View("Thanks",guestResponse);
+        }
+        public ViewResult ListResponses() {
+            return View(Repasitory.Responses.Where(r => r.WillAttend == true));
+        }
+    }
+}
---
+++ Example1/Example1/Models/Repasitory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Example1.Models
+{
+    public class Repasitory
+    {
+        private static List<GuestResponse> responses = new List<GuestResponse>();
+
+        public static IEnumerable<GuestResponse> Responses
+        {
+            get
+            {
+                return responses;
+            }
+        }
+        public static void AddResponse(GuestResponse response)
+        {
+            responses.Add(response);
+        }
+    }
+}
+
